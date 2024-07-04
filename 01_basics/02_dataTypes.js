8c93580d303ab365d8e626823789a5198bf4b520
Saumya250620 <saumya25062001@gmail.com>
2024-07-05 00:56:24 +0530

    Create 02_dataTypes.js
    datatypes, null, url discussed and use strict discussed

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,41 @@
+"use Strict";  // treat all JS code as never version
+// It helps to ensure that your code is cleaner,
+// less error-prone, and more optimized. It 
+// encourages best practices and makes it easier
+// to identify potential issues early in the 
+// development process.
+// it is a restricted variant of JavaScript
+
+// alert(3+3); // we are using nodejs, not browser
+
+/*
+ console.log(3+3) console.log("Saumya")
+ code readability should be high
+
+Two important URLs
+standard documents
+https://tc39.es/ecma262/#sec-intro
+organisation documents
+https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
+
+*/
+
+let name = "saumya";
+let age = 23;
+let isLoggedIn = false;
+let state;
+
+// PRIMITIVE
+// number => 2 to pow 53
+// bigint
+// string
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof age);
+console.log(typeof null);   // object
+console.log(typeof undefined); // undefined
